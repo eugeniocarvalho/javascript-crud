840921d06ebe780dc9c29b3d51c2dfd6b1d3c336
Eugênio Carvalho <eugeniofrcarvalho@gmail.com>
2022-07-31 20:40:09 -0300

    adding data to api

--- projeto_inicial/admin/service/cliente-service.js
+++ projeto_inicial/admin/service/cliente-service.js
@@ -1,10 +1,26 @@
 const listaClientes = () => {
   return fetch(`http://localhost:3000/profile`)
-  .then(resposta => {
-    return resposta.json();
+    .then(resposta => {
+      return resposta.json();
+    });
+}
+
+const criaCliente = (nome, email) => {
+  return fetch(`http://localhost:3000/profile`, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json"
+    },
+    body: JSON.stringify({
+      nome: nome,
+      email: email
+    })
+  }).then(resposta => {
+    return resposta.body;
   });
 }
 
 export const clienteService = {
-  listaClientes
+  listaClientes,
+  criaCliente
 }
