b92a3b8a02ee3e2c02830364c18a426edc60d849
Eugênio Carvalho <eugeniofrcarvalho@gmail.com>
2022-08-06 15:32:11 -0300

    creating messages error

--- projeto_inicial/admin/service/cliente-service.js
+++ projeto_inicial/admin/service/cliente-service.js
@@ -1,7 +1,9 @@
 const listaClientes = () => {
-  return fetch(`http://localhost:3000/profile`)
+  return fetch(`http://localhost:3000/profle`)
     .then(resposta => {
-      return resposta.json();
+      if (resposta.ok)
+        return resposta.json();
+      throw new Error("Erro ao listar clientes");
     });
 }
 
@@ -16,20 +18,29 @@ const criaCliente = (nome, email) => {
       email: email
     })
   }).then(resposta => {
-    return resposta.body;
+    if (resposta.ok)
+      return resposta.body;
+    else
+      throw new Error("Erro ao criar cliente");
   });
 }
 
 const removeCliente = (id) => {
   return fetch(`http://localhost:3000/profile/${id}`, {
     method: "DELETE"
+  }).then(resposta => {
+    if (!resposta.ok)
+      throw new Error("Erro ao deletar cliente");
   });
 }
 
 const detalhaCliente = (id) => {
   return fetch(`http://localhost:3000/profile/${id}`)
     .then(resposta => {
-      return resposta.json();
+      if (resposta.ok)
+        return resposta.json();
+      else
+        throw new Error("Erro ao detalhar clientes");
     });
 }
 
@@ -44,9 +55,12 @@ const atualizaCliente = (id, nome, email) => {
       email: email,
     })
   })
-  .then(resposta => {
-    return resposta.json();
-  });
+    .then(resposta => {
+      if (resposta.ok)
+        return resposta.json();
+      else
+        throw new Error("Erro ao atualizar clientes");
+    });
 }
 
 export const clienteService = {
